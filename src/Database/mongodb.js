fcf880aff61f35502f5a5e014b08b2448e71648e
ankit-evra <ankit@evramedia.com>
2024-03-12 14:19:24 +0530

    push

--- src/Database/mongodb.js
+++ src/Database/mongodb.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.zhgpqr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.zhgpqr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://akankitkumar3896:"+process.env.DB_PASSWORD+"@cluster0.zhgpqr3.mongodb.net/";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
