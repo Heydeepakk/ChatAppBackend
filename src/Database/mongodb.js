bca3d0862241a49d81430074aac1ef91cd383f5d
Gauravk <you@example.com>
2024-03-12 17:53:34 +0530

    asd

--- src/Database/mongodb.js
+++ src/Database/mongodb.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@chat-app.qckmeog.mongodb.net/chat-app?retryWrites=true&w=majority&appName=chat-app";
+// const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster.44jqtrj.mongodb.net/Chatapp?retryWrites=true&w=majority&appName=Cluster";
 
 run = () =>{
   mongoose.connect(uri)
