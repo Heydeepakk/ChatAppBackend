6ec2a8ef2ecd7f40ab5fb81f6a4f938e9fda0df4
ankit-evra <ankit@evramedia.com>
2024-03-13 09:59:14 +0530

    push

--- src/Database/mongodb.js
+++ src/Database/mongodb.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@chat-app.qckmeog.mongodb.net/chat-app?retryWrites=true&w=majority&appName=chat-app";
-// const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster.44jqtrj.mongodb.net/Chatapp?retryWrites=true&w=majority&appName=Cluster";
 
 run = () =>{
   mongoose.connect(uri)
@@ -8,7 +7,7 @@ run = () =>{
     console.log("Connected to MongoDB")
   )
   .catch(err =>{
-    console.log('Failed to connect to connect ')
+    console.log('Failed to connect')
   }
   )
 }
