7958376fe4a9cdefc83de664ae046efca220848a
ankit-evra <ankit@evramedia.com>
2024-03-14 10:29:41 +0530

    push

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ const upload = multer();
 
 //use of app
 const app = express();
-// app.use(cors());
-app.use(cors({origin: true, credentials: true}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
