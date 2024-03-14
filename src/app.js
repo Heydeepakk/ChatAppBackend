c74468b8fdb1d0f6d297f3a208861c39983fcc0d
ankit-evra <ankit@evramedia.com>
2024-03-14 11:04:04 +0530

    push

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const upload = multer();
 //use of app
 const app = express();
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 // app.use(express.urlencoded({ extended: true}));
 
 //here we will put our routes
