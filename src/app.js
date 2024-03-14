9acf27a70f4f25e3e2e3367ac7b0e376d0d5ddf1
ankit-evra <ankit@evramedia.com>
2024-03-14 12:17:05 +0530

    push khhush rho

--- src/app.js
+++ src/app.js
@@ -7,20 +7,6 @@ const upload = multer();
 
 //use of app
 const app = express();
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
