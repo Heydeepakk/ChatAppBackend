10eb82fdada191aacec24da9e845fb4004e29881
ankit-evra <ankit@evramedia.com>
2024-03-14 11:56:11 +0530

    push

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -59,15 +59,6 @@ exports.validateOtp = async (req, res, next) => {
 };
 
 exports.getAllUser = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PATCH, DELETE, OPTIONS"
-  );
   res.json({ data: await userModel.find() });
 };
 
